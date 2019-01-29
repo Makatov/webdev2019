5e75a25a3f743f0188741af47bb952f5a52cfb20
makatov <samakat1999@gmail.com>
2019-01-29 12:42:24 +0600

    Old simple projects

---
+++ Some my old html static projects as a confirmation that i know the basics /www.petro-mbakz/js/map-active.js
@@ -0,0 +1,41 @@
+var map;
+var latlng = new google.maps.LatLng(51.1807167, 71.4002556);
+var stylez = [{
+    featureType: "all",
+    elementType: "all",
+    stylers: [{
+        saturation: -30
+            }]
+        }];
+var mapOptions = {
+    zoom: 16,
+    center: latlng,
+    scrollwheel: false,
+    scaleControl: false,
+    disableDefaultUI: true,
+    mapTypeControlOptions: {
+        mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'gMap']
+    }
+};
+map = new google.maps.Map(document.getElementById("googleMap"), mapOptions);
+var geocoder_map = new google.maps.Geocoder();
+var address = 'Astana, ul. Maskeu 20';
+geocoder_map.geocode({
+    'address': address
+}, function (results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+        map.setCenter(results[0].geometry.location);
+        var marker = new google.maps.Marker({
+            map: map,
+            icon: 'img/core-img/map.png',
+            position: map.getCenter()
+        });
+    } else {
+        alert("Geocode was not successful for the following reason: " + status);
+    }
+});
+var mapType = new google.maps.StyledMapType(stylez, {
+    name: "Grayscale"
+});
+map.mapTypes.set('gMap', mapType);
+map.setMapTypeId('gMap');
